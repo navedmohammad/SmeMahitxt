4b39ab8f61b613e88ac6b69da6c31bbafefadf43
Mohammadnaved Naved <36394261+navedmohammad@users.noreply.github.com>
2024-07-26 11:04:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "7347471600:AAFWOe1YpS1Ap_AqWpLNHZNY4V-Tkj3nUTE",
+             bot_token= "7273979620:AAEPirGpjUmzuENOu5cKrEwzjxhRD_ZkeZ4",
              api_id= 22475741,
              api_hash= "1a217be71a0225e0a678af286c211f8a")
 
