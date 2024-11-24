848ddf0122afbd379e0473a63b3056a10cc0e38a
Mohammadnaved Naved <36394261+navedmohammad@users.noreply.github.com>
2024-11-24 10:03:14 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 20483216
-api_hash = "2518170d3dd939b3f2893cbOaae805c4"
-bot_token = "7942365406:AAEhqRL_6PblpO5gXvH9EFvBamDMRsgKe2o"
+api_id = 22475741
+api_hash = "1a217be71a0225e0a678af286c211f8a"
+bot_token = "7935803756:AAF0dAnCgftWoUCTxJlT0ogw-hpk6x2jSO8"
 
